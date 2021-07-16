425bc0a31064a9c4df1f34cc62a092e4fb282a15
Dylan <dcren2010@gmail.com>
2021-07-16 15:22:14 -0400

    indicate the JSX.Element[] type

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,36 +1,32 @@
-import { render } from '@testing-library/react'
-import React from 'react'
+import React from 'react';
 
 interface IProps {
 	people: {
-		name: string
-		age: number
-		url: string
-		note?: string
+		name: string;
+		age: number;
+		url: string;
+		note?: string;
 	}[];
 }
 
 // Specific the component type with the props type: React.FC<IProps>
+
 const List: React.FC<IProps> = ({ people }) => {
-	
-	const renderList = () => (
-		people.map(({name, age, url, note}) => (
+
+	// we can hover over the "renderList" to find its type: JSX.Element[]
+	const renderList = (): JSX.Element[] =>
+		people.map(({ name, age, url, note }) => (
 			<li key={name} className='List'>
 				<div className='List-header'>
-					<img className='List-img' src={url} alt="" />
+					<img className='List-img' src={url} alt='' />
 					<h2>{name}</h2>
 				</div>
 				<p>{age} years old</p>
 				<p className='List-note'>{note}</p>
 			</li>
-		))
-	)
+		));
 
-	return (
-		<ul>
-			{renderList()}
-		</ul>
-	)
-}
+	return <ul>{renderList()}</ul>;
+};
 
-export default List
+export default List;
