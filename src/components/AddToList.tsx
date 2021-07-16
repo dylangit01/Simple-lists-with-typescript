68a0aacfe2252a9513232b880d03c2b14f31726f
Dylan <dcren2010@gmail.com>
2021-07-16 18:49:14 -0400

    add input and textarea to AddToList component

--- src/components/AddToList.tsx
+++ src/components/AddToList.tsx
@@ -1,11 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 const AddToList = () => {
-	return (
-		<div>
-			AddToList component
-		</div>
-	)
-}
+	const [input, setInput] = useState({
+		name: '',
+		age: '',
+		note: '',
+		img: '',
+	});
 
-export default AddToList
+	const { name, age, note, img } = input;
+
+	const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+		setInput({
+			...input,
+			[e.target.name]: e.target.value,
+		});
+	};
+
+	return (
+		<div className='AddToList'>
+			<input
+				value={name}
+				onChange={(e) => handleChange(e)}
+				type='text'
+				placeholder='Name'
+				name='name'
+				className='AddToList-input'
+			/>
+			<input
+				value={age}
+				onChange={(e) => handleChange(e)}
+				type='text'
+				placeholder='Age'
+				name='age'
+				className='AddToList-input'
+			/>
+			<input
+				value={img}
+				onChange={(e) => handleChange(e)}
+				type='text'
+				placeholder='Url'
+				name='img'
+				className='AddToList-input'
+			/>
+			<textarea
+				value={note}
+				onChange={(e) => handleChange(e)}
+				placeholder='Note'
+				name='note'
+				className='AddToList-input'
+			/>
+		</div>
+	);
+};
+
+export default AddToList;
