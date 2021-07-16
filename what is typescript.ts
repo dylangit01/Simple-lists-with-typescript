1198439acaaa57f77955b7bd3daa464978b395b9
Dylan <dcren2010@gmail.com>
2021-07-16 14:41:15 -0400

    update text

--- what is typescript.ts
+++ what is typescript.ts
@@ -6,24 +6,17 @@ console.log(myName);
 
 /* Since we defined the type to the variable, whenever if we forget its type or try to change its type, typescript will give us error message
  So the benefits of typescript are:
+
  1. Catch errors early and easily in development
  2. Servers as Documentation: essentially when we define types in react application with many pages and components, it helps to understand what types that are involved for a certain prop or a certain state, and to understand what this application is trying to do
+
  */
 
 // Install react with typescript template: npx create-react-app --template typescript ./
 
+// Using "interface" to define the types before the functional component
 
 
-/* For objects you can use the spread operator to use prevState within your setState call.
 
-const [stateObject, setObjectState] = useState({
-	firstKey: '',
-	secondKey: '',
-});
 
-setObjectState((prevState) => ({
-	...prevState,
-	secondKey: 'value',
-}));
 
-*/
