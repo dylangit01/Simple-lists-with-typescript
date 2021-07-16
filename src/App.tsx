a1f40f8c32adb415914d3b09120008929f14aa03
Dylan <dcren2010@gmail.com>
2021-07-16 14:34:50 -0400

    add people

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  
+  const [people, setPeople] = useState([
+		{
+			name: 'James Born',
+			url: '',
+			age: 35,
+			note: 'Welcome to the money world',
+		},
+		{
+			name: 'John Dow',
+			url: '',
+			age: 37,
+		},
+	]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People Invited to my Party</h1>
     </div>
   );
 }
